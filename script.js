98428942713963d0ef6278bef43a707fd96aa084
Arun Yadav <ay20552291@gmail.com>
2025-12-31 17:57:26 +0530

    s

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menu-button");
 const menuClose = document.getElementById("menu-close");
 const nav = document.querySelector("nav");
 
+/* MOBILE MENU */
 menuBtn.addEventListener("click", () => {
   nav.classList.add("open");
 });
@@ -9,3 +10,17 @@ menuBtn.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   nav.classList.remove("open");
 });
+
+/* HERO IMAGE SWITCH (FOR CYPRESS) */
+const heroImg = document.querySelector(".hero-img img");
+
+function updateHeroImage() {
+  if (window.innerWidth < 1000) {
+    heroImg.src = "./assets/image-web-3-mobile.jpg";
+  } else {
+    heroImg.src = "./assets/image-web-3-desktop.jpg";
+  }
+}
+
+updateHeroImage();
+window.addEventListener("resize", updateHeroImage);
